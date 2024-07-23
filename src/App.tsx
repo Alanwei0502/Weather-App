6fc3de227daaca21d7a81f91ad072b2d73e7a9ad
Alan Huang <alans40609@gmail.com>
2024-07-23 09:42:47 +0800

    fix the first loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,13 @@
-import { useEffect, useState } from 'react';
-import OpenWeather from './api/openWeather.api';
-import { setCity, setIsLoading } from './store/slices/weather.slice';
 import Header from './components/Header';
 import WeatherDashboard from './components/WeatherDashboard';
 import CityCardList from './components/CityCardList';
 import Footer from './components/Footer';
 import Spinner from './components/Sipnner';
-import { useAppDispatch } from './hooks';
 
 function App() {
-  const dispatch = useAppDispatch();
-  const [prompt, setPrompt] = useState('');
-
-  // Get user's location permission and set the weather information
-  useEffect(() => {
-    const setUserLocalWeather = async (position: GeolocationPosition) => {
-      dispatch(setIsLoading(true));
-      const { latitude, longitude } = position.coords;
-      const cities = await OpenWeather.getCityInfoByCoords({ lat: latitude, lon: longitude });
-      dispatch(setCity(cities[0]));
-      dispatch(setIsLoading(false));
-    };
-
-    if (navigator.permissions) {
-      navigator.permissions.query({ name: 'geolocation' }).then((result) => {
-        if (result.state === 'granted') {
-          navigator.geolocation.getCurrentPosition(setUserLocalWeather);
-        } else if (result.state === 'prompt' || result.state === 'denied') {
-          setPrompt('Please allow location access to get the weather information.');
-        }
-      });
-    } else {
-      navigator.geolocation.getCurrentPosition(setUserLocalWeather);
-    }
-  }, [dispatch]);
-
   return (
     <div className='mx-auto max-w-screen-md'>
       <Header />
-      {prompt && <p className='fixed z-10 text-center top-1/2 -translate-y-1/2'>{prompt}</p>}
       <main className='text-shadow-sm px-2 pb-20 mx-auto max-w-screen-md min-h-screen relative select-none overflow-y-scroll'>
         <WeatherDashboard />
         <CityCardList />
--- src/components/WeatherDashboard.tsx
+++ src/components/WeatherDashboard.tsx
@@ -1,17 +1,45 @@
-import { useAppSelector } from '../hooks';
+import { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../hooks';
 import CurrentWeather from './CurrentWeather';
 import Forecast from './Forecast';
+import { setCity, setIsLoading } from '../store/slices/weather.slice';
+import OpenWeather from '../api/openWeather.api';
 
 const WeatherDashboard = () => {
+  const dispatch = useAppDispatch();
+  const [isShowPrompt, setIsShowPrompt] = useState(false);
+
+  const city = useAppSelector(state => state.weather.city);
   const backgroundColor = useAppSelector(state => state.weather.backgroundColor);
   const isMenuOpen = useAppSelector(state => state.weather.isMenuOpen);
+
+  // Get user's geolocation and show the local weather information
+  useEffect(() => {
+    dispatch(setIsLoading(true));
+
+    navigator.geolocation.getCurrentPosition(
+      async (position: GeolocationPosition) => {
+        const { latitude, longitude } = position.coords;
+        const cities = await OpenWeather.getCityInfoByCoords({ lat: latitude, lon: longitude });
+        dispatch(setCity(cities[0]));
+        dispatch(setIsLoading(false));
+      },
+      () => {
+        setIsShowPrompt(true);
+        dispatch(setIsLoading(false));
+      }
+    );
+  }, [dispatch]);
+
   return (
     <>
       <div aria-label="day-bg" className={`absolute inset-0 transition-opacity duration-1000 ease-in-out bg-gradient-to-t from-sky-800 via-sky-650 to-sky-500 ${backgroundColor === 'day' ? 'opacity-100' : 'opacity-0'}`}
       />
       <div aria-label="night-bg" className={`absolute inset-0 transition-opacity duration-1000 ease-in-out bg-gradient-to-t from-slate-950 via-indigo-950 to-blue-900 ${backgroundColor === 'night' ? 'opacity-100' : 'opacity-0'}`}
       />
-      <section className={`relative duration-100 ease-in-out ${!isMenuOpen ? 'block' : 'hidden'}`} >
+      <p className={`absolute z-10 text-center w-full left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 ${!city && !isMenuOpen && isShowPrompt ? '' : 'hidden'}`}>Please allow location access to get the local weather information.</p>
+
+      <section className={`relative duration-100 ease-in-out ${!isMenuOpen ? '' : 'hidden'}`} >
         <CurrentWeather />
         <Forecast />
       </section>
